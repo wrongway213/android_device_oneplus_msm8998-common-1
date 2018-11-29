55c03bc593ce82894c91d6b0d24a93668718ccca
REV3NT3CH <rev3nt3ch@gmail.com>
2018-11-29 15:55:10 -0800

    Change from oppo common to extras

--- common.mk
+++ common.mk
@@ -487,5 +487,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+# Inherit Oneplus Extras
+$(call inherit-product, device/oneplus/extras/common.mk)
